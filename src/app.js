9b9a40718f4abd1bb3914d15fc7b81c5fc883284
Eduardo Ribeiro <166298206+EIAHRJAY@users.noreply.github.com>
2024-05-31 09:35:04 +0000

    primer Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const suits = ["♦", "♥", "♠", "♣"];
+
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function generateRandomCard() {
+  const value = getRandomElement(values);
+  const suit = getRandomElement(suits);
+
+  document.querySelector(".card-value").textContent = value;
+  document.querySelector(".card-value").style.color =
+    suit === "♥" || suit === "♦" ? "red" : "black";
+
+  document.querySelectorAll(".card-suit").forEach(element => {
+    element.textContent = suit;
+    element.style.color = suit === "♥" || suit === "♦" ? "red" : "black";
+  });
+}
+
+window.onload = generateRandomCard;
+
+document
+  .getElementById("generate-card")
+  .addEventListener("click", generateRandomCard);
+
+setInterval(generateRandomCard, 10000);
